b6a8fb4dc10f3e2cb76927f4071aef777df5bbba
Daniel Do <dodaniel006@gmail.com>
2025-11-08 23:11:51 -0800

    Add TrueFalseQuestion component

--- src/components/Questions/TrueFalseQuestion.tsx
+++ src/components/Questions/TrueFalseQuestion.tsx
@@ -1,9 +1,47 @@
 import type Question from "../../types/question.js";
-import { Box } from "ink";
+import { Box, Text } from "ink";
 import React from "react";
+import InputLine from "../inputline.js";
+import SelectInput from 'ink-select-input';
+import Phase from "../../types/phase.js";
 
-const TrueFalseQuestion = ({ data }: { data: Question }) => {
-  return <Box></Box>;
+const TrueFalseQuestion = ({
+  currentQNum,
+  setCurrentQNum,
+  writeAnswer,
+  setPhase,
+  numQuestions,
+}: {
+  currentQNum: number;
+  setCurrentQNum: React.Dispatch<React.SetStateAction<number>>;
+  writeAnswer: (answer: string) => void;
+  setPhase: React.Dispatch<React.SetStateAction<Phase>>;
+  numQuestions: number;
+}) => {
+
+  const handleSelect = (item: { label: string; value: string }) => {
+    writeAnswer(item.value);
+    if (currentQNum + 1 >= numQuestions) {
+      setPhase("results");
+      return;
+    }
+    setCurrentQNum((prev) => prev + 1);
+  };
+
+  const items = [
+    {
+      label: 'True',
+      value: 'true'
+    },
+    {
+      label: 'False',
+      value: 'false'
+    }
+  ];
+
+  return (
+    <SelectInput items={items} onSelect={handleSelect} />
+  );
 };
 
 export default TrueFalseQuestion;
